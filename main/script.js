0a01ceafa08908474fe966cba5cef9ab06b74243
Anatoly Sadovsky <85368616+DixiePip@users.noreply.github.com>
2022-12-13 12:21:40 +0300

    Add files via upload

---
+++ main/script.js
@@ -0,0 +1,12 @@
+
+// Jquery slider
+
+$(document).ready(function() {
+    $('.slider').slick({
+        dots: true, //точки снизу
+        speed: 1000, //скорость переключения слайла
+        autoplay: true, //автопроигрование
+        autoplaySpeed: 2000 //промежуток
+    });
+});
+
